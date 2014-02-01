acddf7876138b6a47755a90449b5aec16e46470f
nima732 <nima7321@gmail.com>
2014-02-01 13:50:35 +0100

    .

---
+++ Anti-spy/src/draw/SingleTouchEventView.java
@@ -0,0 +1,65 @@
+package draw;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.anti_spy.R;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+
+public class SingleTouchEventView extends View {
+	  private Paint paint = new Paint();
+	  private Path path = new Path();
+
+	  public SingleTouchEventView(Context context, AttributeSet attrs) {
+	    super(context, attrs);
+
+	    paint.setAntiAlias(true);
+	    paint.setStrokeWidth(6f);
+	    paint.setColor(Color.BLACK);
+	    paint.setStyle(Paint.Style.STROKE);
+	    paint.setStrokeJoin(Paint.Join.ROUND);
+	  }
+
+	  @Override
+	  protected void onDraw(Canvas canvas) {
+	    canvas.drawPath(path, paint);
+	  }
+
+	  @Override
+	  public boolean onTouchEvent(MotionEvent event) {
+	    float eventX = event.getX();
+	    float eventY = event.getY();
+
+	    switch (event.getAction()) {
+	    case MotionEvent.ACTION_DOWN:
+	      path.moveTo(eventX, eventY);
+	      return true;
+	    case MotionEvent.ACTION_MOVE:
+	      path.lineTo(eventX, eventY);
+	      break;
+	    case MotionEvent.ACTION_UP:
+	      // nothing to do
+	      break;
+	    default:
+	      return false;
+	    }
+
+	    // Schedules a repaint.
+	    invalidate();
+	    return true;
+	  }
+	} 
